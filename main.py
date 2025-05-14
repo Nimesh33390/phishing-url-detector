e4546286f391438a56744852bf50fb720ceab6c3
Nimesh <nimesh2767@gmail.com>
2025-05-15 01:47:28 +0530

    Add FastAPI prediction code

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
-# app/main.py
-
 from fastapi import FastAPI
+from tensorflow import keras
+from Feature_Extractor import extract_features
 from pydantic import BaseModel
-import numpy as np
-import tensorflow as tf
 
 app = FastAPI()
 
-# Load model
-model = tf.keras.models.load_model("app/model.h5")
+model = None
 
-# Define input data structure
-class InputData(BaseModel):
-    features: list[float]
+@app.on_event("startup")
+def load_model():
+    global model
+    model = keras.models.load_model("Malicious_URL_Prediction.h5")  # Replace with the actual path to your model file
+
+def predict_url(url, model):
+    url_features = extract_features(url)
+    prediction = model.predict([url_features])
+    probability = prediction[0][0] * 100
+    probability = round(probability, 3)
+    return probability
+
+class URLInput(BaseModel):
+    url: str
 
 @app.post("/predict")
-def predict(data: InputData):
-    input_array = np.array([data.features])  # Add batch dimension
-    prediction = model.predict(input_array)
-    return {"prediction": prediction.tolist()}
+def predict(input: URLInput):
+    probability = predict_url(input.url, model)
+    return {"probability": probability, "message": f"There is {probability}% chance the URL is malicious!"}
